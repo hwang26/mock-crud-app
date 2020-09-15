e5509ba597f1cec2d6e7f18695e5fd4e9d3e8fea
hwang26 <hunterzwang@gmail.com>
2020-09-14 20:13:21 -0400

    Landing page works

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ submitButton.addEventListener("click", submitButtonClick);
 
 const inputArray = document.querySelectorAll(".input-field input");
 
+const labelArray = document.querySelectorAll(".input-field label");
+
+const landingPageData = window.localStorage;
+
+const selectLanding = document.querySelector(".landing");
+
+let fieldCheck = new Array(inputArray.length);
+
+for(let i=0; i<fieldCheck.length; i++){
+ fieldCheck[i] = 0;
+ }
+
 function validationMessage(message, className){
     vMessage = document.createElement("p");
     vMessage.setAttribute("class", className);
@@ -28,18 +40,14 @@ function validationMessage(message, className){
     return vMessage;
 }
 
+let formShow = landingPageData.getItem("formShow");
+if(formShow != "none"){
+    selectLanding.style.display = "flex";
+}
+
 // On submission, we should prevent default behavior and add in how we want the data to be stored
 function submitButtonClick(e){
     e.preventDefault();
- 
-    /*
-        - Validate fields and throw error text if invalid values
-        - Take current values from fields and store them in IndexedDB
-            - Use window.querySelectorAll(...) to grab the fields
-        - Clear out fields
-        - Submission success
-            - Dynamically hide the landing box
-    */
 
     /*
         JS Validation
@@ -49,17 +57,6 @@ function submitButtonClick(e){
         
         - If any conditions are false, print an error message. Otherwise, continue with submission.
     */
-
-   // REWORK logic: Should be FALSE and TRUE when all fields have correct behavior. We can have an array of booleans. 
-   let emptyCheck = new Array(inputArray.length);
-   let numericCheck = new Array(inputArray.length-2);
-
-   for(let i=0; i<emptyCheck.length; i++){
-    emptyCheck[i] = false;
-    }
-    for(let i=0; i<numericCheck.length; i++){
-        numericCheck[i] = true;
-    }
 
     for(let i=0; i<inputArray.length; i++){
 
@@ -79,14 +76,14 @@ function submitButtonClick(e){
                 inputArray[i].insertAdjacentElement('afterend',emptyMessage);
             }
 
-            emptyCheck[i] = true;
+            fieldCheck[i]++;
+
         }
         // If the value is not null and there's currently a validation message
         if(inputArray[i].value != "" && currentMessage != null){
             // Remove the validation message
             currentMessage.remove();
-
-            emptyCheck[i] = false;
+            fieldCheck[i]--;
         }
     }
 
@@ -109,39 +106,36 @@ function submitButtonClick(e){
                 inputArray[i].insertAdjacentElement('afterend',emptyMessage);
             }
 
-            numericCheck[i-2] = false;
+            fieldCheck[i]++;
+
         }
 
         // If its a number and there's currently a validation message
         if(!isNaN(Number(inputArray[i].value)) && currentMessage != null ){
             currentMessage.remove();
-            numericCheck[i-2] = true;
+            fieldCheck[i]--;
         }
     }
 
-    // If the conditions are valid, then we can proceed with submission. emptyCheck should be all false and numericCheck should be all true.
+    // If the conditions are valid, then we can proceed with submission
     let finalCheck = true;
 
-    for(let i=0; i<emptyCheck.length; i++){
-        if(emptyCheck[i]==true){
+    for(let i=0; i<fieldCheck.length; i++){
+        if(fieldCheck[i]!=0){
             finalCheck = false;
         }
-
-    }
-    for(let i=0; i<numericCheck.length; i++){
-        if(numericCheck[i]==false){
-            finalCheck = false;
-        }
-
-    }
-
-    // Proceed with submission
-    if(finalCheck==true){
-        console.log(emptyCheck);
-        console.log(numericCheck);
-        console.log("hi2");
     }
     
+    // Store the values in localStorage
+    if(finalCheck == true){
+        for(let i=0; i<inputArray.length; i++){
+            landingPageData.setItem(labelArray[i].innerText,inputArray[i].value);
+        }
+
+        landingPageData.setItem("formShow", "none");
+
+        // Make the landing box disappear
+        selectLanding.style.display = "none";     
+    }
 
 }
-
